35e36242417db6cc5b583e280ff4178b7b012a08
GreenHornedOwl <daniel.voicu.dev@gmail.com>
2021-03-11 15:47:29 +0200

    update

--- ayur-ui/components/Container.js
+++ ayur-ui/components/Container.js
@@ -4,21 +4,16 @@ const Container = ({children, container, cssClass}) => {
   let [localContainer, setLocalContainer] = useState("container");
   let [config,setConfig] = useState(false)
   useEffect(() => {
-    if(container!==undefined) {
-      if(container === "container") {
+    if(container!==undefined) {      
         setLocalContainer(container);
-      } else {
-        setLocalContainer("container--"+container);
-      }
-      
     }
   }, [])  
   return(
-    <section className={"section__container "+ localContainer  + " " + cssClass}>      
+    <section className={`section__container ${cssClass} ${localContainer === "container" ? "container" : `container--${localContainer}`}`}>      
       <div className="section__content relative">
-        <span onClick={e=>setConfig(!config)} className="ico"><svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 512 512'><circle cx='256' cy='256' r='26'/><circle cx='346' cy='256' r='26'/><circle cx='166' cy='256' r='26'/><path fill='none' stroke='currentColor' strokeLinecap='round' strokeLinejoin='round' strokeWidth='32' d='M160 368L32 256l128-112M352 368l128-112-128-112'/></svg></span>
+        <span onClick={e=>setConfig(!config)} className={"ico"}><svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 512 512'><circle cx='256' cy='256' r='26'/><circle cx='346' cy='256' r='26'/><circle cx='166' cy='256' r='26'/><path fill='none' stroke='currentColor' strokeLinecap='round' strokeLinejoin='round' strokeWidth='32' d='M160 368L32 256l128-112M352 368l128-112-128-112'/></svg></span>
         <div className={styles.config + " border p-1 align-items-center child-mr-2 " + (config ? "inline-flex" : "hidden")}>
-          <select onChange={e=>setLocalContainer(e.currentTarget.value)}>
+          <select onChange={e=>setLocalContainer(e.currentTarget.value)} value={localContainer}>
             <option disabled>Select the container size</option>
             <option value="narrow">Narrow Size</option>
             <option value="container">Default Size</option>
--- ayur-ui/pages/paragraph.js
+++ ayur-ui/pages/paragraph.js
@@ -1,12 +1,44 @@
 import Head from 'next/head'
+import Container from '../components/Container'
+import Heading from '../components/Heading'
+import Paragraph from "../components/Paragraph"
 import BaseLayout from './../components/BaseLayout'
+import styles from "../styles/Paragraph.module.sass"
+import ContentCustom from '../components/Content'
 const paragraph = () => {
   return(
     <div>
        <Head>
-        <title>Commergent UI | Banner</title>       
+        <title>Commergent UI | Paragraph</title>       
       </Head>
-      <BaseLayout></BaseLayout>
+      <BaseLayout>
+        <Container container="narrow" cssClass="font-lg">
+          <Heading style="hr1" weight="superbold" cssClass="w-100 mb-3">The Paragraph</Heading>                 
+        </Container>
+        <Container container="wide" cssClass="mt-4">
+          <Paragraph image="/assets/img/banner-demo.jpg" cssClass="mb-4">
+            <Heading style="hr2" weight="superbold">Lorem ipsum dolor sit amet, consectetur adipiscing elit</Heading>
+            <Heading style="h2" cssClass="font-subheading">Cras convallis hendrerit lectus, non malesuada neque malesuada et</Heading>
+            <div className="text">
+              <ContentCustom size="lg" weight="normal">
+              <p>In convallis, enim non hendrerit blandit, augue nisi feugiat lacus, nec vulputate ligula augue vel magna. Mauris gravida id arcu eu maximus. In eleifend sem massa, vel gravida risus ullamcorper eget. Nullam lorem ipsum, dapibus at porta at, fringilla tristique sapien. Donec nec elit condimentum, eleifend leo vitae, ultrices lorem. Nullam bibendum, arcu quis posuere luctus, libero ligula molestie felis, vitae convallis velit mi eget neque. </p>
+              </ContentCustom>
+            </div>
+            <button type="button" className="btn-fill--brand" aria-label="button">Read more</button>
+          </Paragraph>
+          <Paragraph cssClass="mb-4">
+            <Heading style="hr2" weight="superbold">Lorem ipsum dolor sit amet, consectetur adipiscing elit</Heading>
+            <Heading style="h2" cssClass="font-subheading">Cras convallis hendrerit lectus, non malesuada neque malesuada et</Heading>
+            <div className="text">
+              <ContentCustom size="lg" weight="normal">
+              <p>In convallis, enim non hendrerit blandit, augue nisi feugiat lacus, nec vulputate ligula augue vel magna. Mauris gravida id arcu eu maximus. In eleifend sem massa, vel gravida risus ullamcorper eget. Nullam lorem ipsum, dapibus at porta at, fringilla tristique sapien. Donec nec elit condimentum, eleifend leo vitae, ultrices lorem. Nullam bibendum, arcu quis posuere luctus, libero ligula molestie felis, vitae convallis velit mi eget neque. </p>
+              </ContentCustom>
+            </div>
+            <button type="button" className="btn-fill--brand" aria-label="button">Read more</button>
+          </Paragraph>
+          
+        </Container>
+      </BaseLayout>
     </div>
   )
 }
